bbee5f0e8e75ff63fa499438bf31be0b233cba75
Masad Rayan <masadrayan2002@gmail.com>
2026-06-29 00:45:05 +0600

    updated the catchAsync dunction

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -2,17 +2,18 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 
 export const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (error: any) {
-      console.log(error);
-      res.status(httpStatus.BAD_REQUEST).json({
-        success: false,
-        statusCode: httpStatus.BAD_REQUEST,
-        message: "Failed to create user",
-        error: error.message,
-      });
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            console.log(error);
+
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+                message: "Failed to register user",
+                error: (error as Error).message
+            })
+        }
     }
-  };
-};
+}
