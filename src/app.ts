b777bc378b05e5645b4d09d16903d37d48923392
Masad Rayan <masadrayan2002@gmail.com>
2026-07-01 17:13:16 +0600

    Added Global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { prisma } from "./lib/prisma";
@@ -9,28 +9,32 @@ import { authRoute } from "./module/auth/auth.route";
 import { postRoute } from "./module/posts/post.route";
 import { commentRoute } from "./module/comments/comment.route";
 import { routeNotFoundHandler } from "./middleware/notFound";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
 
-const app : Application = express();
+const app: Application = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: config.app_url,
     credentials: true,
-}))
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.get("/", async(req, res) => {
-    const users = await prisma.user.findMany();
-    res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send("Hello World!");
 });
 
-app.use("/api/users", userRoute)
-app.use("/api/auth", authRoute)
-app.use("/api/posts", postRoute)
-app.use("/api/comments", commentRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
-app.use(routeNotFoundHandler)
+app.use(routeNotFoundHandler);
+
+app.use(globalErrorHandler);
 
 export default app;
