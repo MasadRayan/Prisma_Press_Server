efa651933d45ee958187c75b9f53be0d7245c5a0
Masad Rayan <masadrayan2002@gmail.com>
2026-07-01 22:15:56 +0600

    Added auth middleware to restrict the authorizarion of the subscrption route

--- src/module/subscription/subscription.route.ts
+++ src/module/subscription/subscription.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { subscriptionController } from "./subscription.controller";
+import { auth } from "../../middleware/auth";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/checkout", subscriptionController.createCheckOutSession);
+router.post("/checkout", auth(UserRole.ADMIN, UserRole.AUTHOR, UserRole.USER), subscriptionController.createCheckOutSession);
 
 export const subscriptionRouter = router
