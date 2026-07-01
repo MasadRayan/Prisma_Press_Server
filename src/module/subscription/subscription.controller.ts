daa2c0f92362fa2a5e22e6b58f46ce2427007a28
Masad Rayan <masadrayan2002@gmail.com>
2026-07-01 23:02:57 +0600

    Enhance subscription functionality: add createdAt and updatedAt fields, update user model for subscriptions, and implement checkout session creation with Stripe integration.

--- src/module/subscription/subscription.controller.ts
+++ src/module/subscription/subscription.controller.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { enableCompileCache } from "module";
+import { subscriptionService } from "./subscription.service";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 const createCheckOutSession = catchAsync(async (req: Request, res:Response, next: NextFunction) => {
-
+    const userId = req.user?.id;
+    const result = await subscriptionService.createCheckOutSession(userId as string)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Checkout session created successfully",
+        data: result
+    })
 })
 
 export const subscriptionController = {
--- src/module/subscription/subscription.service.ts
+++ src/module/subscription/subscription.service.ts
@@ -1,5 +1,57 @@
-const createCheckOutSession = async () => {
+import config from "../../config";
+import { prisma } from "../../lib/prisma";
+import { stripe } from "../../lib/stripe";
 
+const createCheckOutSession = async (userId : string) => {
+    const checkoutTrasnsaction = await prisma.$transaction(
+        async(tx) => {
+            const user = await tx.user.findUniqueOrThrow({
+                where: {
+                    id: userId
+                },
+                include: {
+                    subscriptions: true
+                }
+            })
+
+            let stripeCustomerId = user.subscriptions?.stripeCustomerId;
+
+            if (!stripeCustomerId) {
+                //new customer, create a new stripe customer
+                const customer = await stripe.customers.create({
+                    email : user.email,
+                    name: user.name,
+                    metadata: {
+                        userId : user.id
+                    }
+                })
+
+                stripeCustomerId = customer.id
+            }
+
+            const session = await stripe.checkout.sessions.create({
+                line_items: [{
+                    price: config.stripe_product_price_id,
+                    quantity: 1
+                }],
+                mode: "subscription",
+                customer : stripeCustomerId,
+                payment_method_types: ["card"],
+                success_url: `${config.app_url}/premium/success`,
+                cancel_url: `${config.app_url}/payment/cancel`,
+                metadata: {
+                    userId: user.id
+                }
+            })
+
+            return session.url
+        }
+        
+    )
+
+    return {
+        paymentURL : checkoutTrasnsaction
+    }
 }
 
 
