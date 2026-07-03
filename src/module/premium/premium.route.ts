6dccf7ec48a8588cb4a31fc727ccb02cf0c2b842
Masad Rayan <masadrayan2002@gmail.com>
2026-07-03 17:43:28 +0600

    Added get All premium POst api

--- src/module/premium/premium.route.ts
+++ src/module/premium/premium.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { premiumController } from "./premium.controller";
+import { auth } from "../../middleware/auth";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router();
-router.get("/", premiumController.getAllPremiumPosts)
+router.get("/", auth(UserRole.ADMIN, UserRole.AUTHOR, UserRole.USER), premiumController.getAllPremiumPosts)
 
 export const premiumRoute = router
--- src/module/premium/premium.service.ts
+++ src/module/premium/premium.service.ts
@@ -1,5 +1,12 @@
-const getAllPremiumPostFromDb = async (userId : string) => {
+import { prisma } from "../../lib/prisma";
 
+const getAllPremiumPostFromDb = async (userId : string) => {
+    const result = await prisma.post.findMany({
+        where: {
+            isPremium: true
+        }
+    })
+    return result;
 }
 
 export const premiumService = {
